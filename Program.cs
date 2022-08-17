ce9fb11a3776e85f03b5f877073e3231627be50b
alexander-sil <93575945+alexander-sil@users.noreply.github.com>
2022-08-17 10:10:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace AlgoHomework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			TestAll();
+            Console.ReadKey(true);
+        }
+		
+		static void TestAll()
+		{
+            Console.WriteLine("Fibonacci -> " + TestFibonacci(10, 55, false));
+
+            Console.WriteLine("Fibonacci (recursive) -> " + TestFibonacci(10, 55, true));
+			
+            Console.WriteLine("Prime number test #1 -> " + TestFlowchart(997, true));
+			
+            Console.WriteLine("Prime number test #2 -> " + TestFlowchart(1000, false));
+		}
+		
+        static bool TestFibonacci(int sourceNumber, int expectedNumber, bool recursive)
+        {
+            int computedNumber = default;
+
+            if (recursive)
+            {
+                computedNumber = Fibonacci.FibRec(sourceNumber);
+            }
+            else
+            {
+                computedNumber = Fibonacci.FibStd(sourceNumber);
+            }
+
+            return computedNumber == expectedNumber;
+        }
+		
+		static bool TestFlowchart(int number, bool expectedResult)
+		{
+			bool computedResult = FlowChart.IsPrime(number);
+			return computedResult == expectedResult;
+		}
+    }
+}
