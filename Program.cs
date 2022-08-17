5d3920783c2539bc2ab0c2fe8b64359edcd1f9c3
alexander-sil <93575945+alexander-sil@users.noreply.github.com>
2022-08-17 09:57:49 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,41 +6,41 @@ namespace AlgoHomework1
     {
         static void Main(string[] args)
         {
-			TestAll();
-            Console.ReadKey(true);
+	      TestAll();
+              Console.ReadKey(true);
         }
 		
-		static void TestAll()
-		{
-            Console.WriteLine("Fibonacci -> " + TestFibonacci(10, 55, false));
+	 static void TestAll()
+	 {
+              Console.WriteLine("Fibonacci -> " + TestFibonacci(10, 55, false));
 
-            Console.WriteLine("Fibonacci (recursive) -> " + TestFibonacci(10, 55, true));
+              Console.WriteLine("Fibonacci (recursive) -> " + TestFibonacci(10, 55, true));
 			
-            Console.WriteLine("Prime number test #1 -> " + TestFlowchart(997, true));
+              Console.WriteLine("Prime number test #1 -> " + TestFlowchart(997, true));
 			
-            Console.WriteLine("Prime number test #2 -> " + TestFlowchart(1000, false));
-		}
+              Console.WriteLine("Prime number test #2 -> " + TestFlowchart(1000, false));
+        }
 		
         static bool TestFibonacci(int sourceNumber, int expectedNumber, bool recursive)
         {
-            int computedNumber = default;
+             int computedNumber = default;
 
-            if (recursive)
-            {
-                computedNumber = Fibonacci.FibRec(sourceNumber);
-            }
-            else
-            {
-                computedNumber = Fibonacci.FibStd(sourceNumber);
-            }
+             if (recursive)
+             {
+                  computedNumber = Fibonacci.FibRec(sourceNumber);
+             }
+             else
+             {
+                  computedNumber = Fibonacci.FibStd(sourceNumber);
+             } 
 
-            return computedNumber == expectedNumber;
+             return computedNumber == expectedNumber;
         }
 		
-		static bool TestFlowchart(int number, bool expectedResult)
-		{
-			bool computedResult = FlowChart.IsPrime(number);
-			return computedResult == expectedResult;
-		}
+        static bool TestFlowchart(int number, bool expectedResult)
+	{
+	     bool computedResult = FlowChart.IsPrime(number);
+	     return computedResult == expectedResult;
+	}
     }
 }
